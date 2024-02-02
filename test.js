cb51dd5c6b0b3fddbbb0557f4cf86a3c09746bbf
Jack <jpgundrum@wisc.edu>
2024-02-02 11:53:46 -0600

    refactor update

--- test.js
+++ test.js
@@ -12,107 +12,89 @@ const OWNER = process.env.OWNER;
 
 const agung_base_url = "wss://wsspc1-qa.agung.peaq.network";
 
-// creates new agung sdk instance
-const sdk = await Sdk.createInstance({
-    baseUrl: agung_base_url,
-    seed: MNEMONIC,
-});
-
-// used to convert Uint8Array to reabable string
-const toHexString = bytes => 
-    bytes.reduce((str, byte) => str + byte.toString(16).padStart(2, '0'), '');
 
 // to generate a random mnemonic... however it lacks funding
 // const generateMnemonicSeed = async () => {
 //     return mnemonicGenerate();
 //   };
 
+// used to convert Uint8Array to reabable string
+const toHexString = bytes => 
+    bytes.reduce((str, byte) => str + byte.toString(16).padStart(2, '0'), '');
 
+// creates new agung sdk instance
+async function createSdkInstance(){
+    if (!MNEMONIC) throw new Error("MNEMONIC is not defined in .env file");
+    await cryptoWaitReady();
+    const sdk = await Sdk.createInstance({
+        baseUrl: agung_base_url,
+        seed: MNEMONIC,
+    });
+    return sdk;
+}
 
 // creates new decentralized ID based on the name passed
-const createDID = async (name) => {
+const createDID = async (sdk, name) => {
     const { hash } = await sdk.did.create({
-        name,
+        name: name,
     });
-    return hash;
+    const didString = "0x" + toHexString(hash);
+    return didString;
 }
 
 // reads the previously created DID name to retrieve information linked
-const readDID = async (name) => {
+const readDID = async (sdk, name) => {
    // DEBUGGING NOTES:
    // cannot have {} around did var, but must have brackets around name... confusing when looking at peaq docs
-    const did = await sdk.did.read({name});
+    const did = await sdk.did.read({name: name});
     return did;
 }
 
 // creates role for the address that is linked to this sdk instance
-const createRole = async (roleName) => {
+const createRole = async (sdk, roleName) => {
     const { roleId: createdRoleId } = await sdk.rbac.createRole({
-        roleName,
+        roleName: roleName,
       });
     return createdRoleId;
 };
 
 // fetches role information for the roleId passed
-const fetchRole = async (roleId) => {
-    const owner = OWNER;
+const fetchRole = async (sdk, roleId) => {
+    if (!OWNER) throw new Error("OWNER is not defined in .env file");
     const role = await sdk.rbac.fetchRole({
-        owner,
-        roleId,
+        owner: OWNER,
+        roleId: roleId,
       });
     return role;
 };
 
-
 async function main() {
+    const sdk = await createSdkInstance();
     await sdk.connect();
 
     const name = 'myDID';
     const roleName = "myrole";
 
-    // create new Decentralized Id
-    await createDID(name)
-    .then((bytesHash) => {
-        const didJson = {
-            hash: "0x" + toHexString(bytesHash)
-        };
-        console.log(`\nDID address: ${didJson.hash}\n`);
-    })
-    .catch((error) => {
-        console.error(`Error creating DID address: ${error}`);
-    });
 
-    // read the previously created Decentralized Id
-    await readDID(name)
-    .then((did) => {
-        console.log(`DID data: \n${JSON.stringify(did)}\n`);
-    })
-    .catch((error) => {
-        console.error(`Error reading DID data: ${error}`);
-    });
+    try {
+        const decentralizedID = await createDID(sdk, name);
+        console.log(`\nDID address: ${decentralizedID}\n`);
 
-    let rID;
-    // create a new role for the address at this sdk instance
-    await createRole(roleName)
-     .then((roleId) => {
-        rID = roleId;
-         console.log(`Created role Id: ${roleId}\n`);
-     })
-     .catch((error) => {
-         console.error(`Error creating new role: ${error}`);
-     });
+        const didInfo = await readDID(sdk, name);
+        console.log(`DID data: \n${JSON.stringify(didInfo)}\n`);
 
-     // fetch role information preivously created
-     await fetchRole(rID)
-     .then((role) => {
-         console.log(`Fetched role: ${JSON.stringify(role)}\n`);
-     })
-     .catch((error) => {
-         console.error(`Error fetching role: ${error}`);
-     });
+        const roleID = await createRole(sdk, roleName);
+        console.log(`Created role Id: ${roleID}\n`);
 
-    // disconnect from sdk once your are completed
-    await sdk.disconnect();
+        const role = await fetchRole(sdk, roleID);
+        console.log(`Fetched role: ${JSON.stringify(role)}\n`);
+
+    }
+    catch (error) {
+        console.error(error);
+    } finally {
+        await sdk.disconnect();
+    }
 }
 
 main().catch((error) => {
